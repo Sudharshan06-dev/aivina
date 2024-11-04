bb3283b99d8994cc5eb69c9ab07bf5a8fd8a0112
Sudharshan Madhavan <msudharshan06@gmail.com>
2024-11-03 17:44:29 -0800

    1) Login Setup done, 2) Header, Footer added 3) Images Issue Fix - Added Toaster

---
+++ ui/src/environment.ts
@@ -0,0 +1,11 @@
+export const APP_NAME = 'Aivina'
+
+export const APP_TITLE = 'Aivina - Agile Project Management Tool';
+
+export const API_PREFIX : string = 'api/v1';
+
+export const LARAVEL_ROUTE : string = 'http://localhost:8000/' + API_PREFIX;
+
+export const JAVA_ROUTE : string = 'http://localhost:8001/' + API_PREFIX;
+
+export const NODE_ROUTE : string = 'http://localhost:8002/' + API_PREFIX;
